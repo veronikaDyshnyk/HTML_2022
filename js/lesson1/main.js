f163098eb46ce0c0cec0405b2cf8b5bf920bc768
veronikaDyshnyk <veronika199942@gmail.com>
2022-04-19 22:39:28 +0300

    finished home task : html2, js1

---
+++ js/lesson1/main.js
@@ -0,0 +1,109 @@
+/*
+- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+    Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+- Створити об'єкт book з наступними полями :
+назва, (тип string)
+кількість сторінок (числовий тип),
+жанр (string)
+
+- Створити об'єкт book з наступними полями :
+назва, (тип string)
+кількість сторінок (числовий тип),
+жанр (string)
+автори (тип - масив, кожен елемент масиву - це стрінга)
+
+- Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+    let a = 100; let b = '100'; let c = true;
+
+- Створити масив об'єктів з назвою books з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт*/
+
+
+//
+// let a1= 'hello';
+// let a2= 'owu';
+// let a3= 'com';
+// let a4= 'ua';
+// let a5= 1;
+// let a6= 10;
+// let a7= -999;
+// let a8= 123;
+// let a9= 3.14;
+// let a10= 2.7;
+// let a11= 16;
+// let a12= true;
+// let a13= false;
+//
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+// console.log(a11);
+// console.log(a12);
+// console.log(a13);
+//
+//
+// let book1 = {
+//     title: 'Eat that Frog',
+//     pages: 100,
+//     genre: 'studying literature'
+// };
+//
+// let book2 = {
+//     title: '1984.Animal Farm',
+//     pages: 416 ,
+//     genre: 'Political satire' ,
+//     authors: [
+//         'George',
+//         'Orwell'
+//     ]
+// }
+
+// let firstName = 'veronika';
+// let middleName = 'ivanivna';
+// let lastName = 'dyshnyk';
+//
+// //let person = firstName + ' ' +middleName + ' ' + lastName;
+// let person = `${firstName} ${middleName} ${lastName}`;
+// console.log(person);
+
+// let firsName = prompt('enter your first name');
+// let middleName = prompt('enter your middle name');
+// let lastName = prompt('enter your last name');
+//
+// let person = `${firsName} ${middleName} ${lastName}`;
+//
+// console.log(person);
+
+//
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// let books = [
+//     {title: 'nudes',pages:100, genre:'photo',authors: ['author1','author2', "author3"]},
+//     {title: 'texts',pages:102, genre:'drama',authors: ['author1','author2', "author3"]},
+//     {title: 'cats',pages:102, genre:'photo',authors: ['author1','author2', "author3"]},
+//     {title: 'borshch',pages:11, genre:'food',authors: ['author1','author2', "author3"]}
+// ];
+//
+// console.log(books[0]);
+// console.log(books[1]);
+// console.log(books[2]);
+// console.log(books[3]);
+//
+
