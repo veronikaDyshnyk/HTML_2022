b4748d29644e8c896578332bf1c4e65666687ead
veronikaDyshnyk <veronika199942@gmail.com>
2022-05-18 00:19:51 +0300

    js6

---
+++ js/lesson6/main.js
@@ -0,0 +1,302 @@
+// - Знайти та вивести довижину настипних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+// let str1 = 'hello world';
+// console.log(str1.length);
+// let str2 = 'lorem ipsum';
+// console.log(str2.length);
+// let str3 = 'javascript is cool';
+// console.log(str3.length);
+
+// - Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+// let str1 = 'hello world';
+// let str2 = 'lorem ipsum';
+// let str3 = 'javascript is cool';
+//
+// console.log(str1.toUpperCase());
+// console.log(str2.toUpperCase());
+// console.log(str3.toUpperCase());
+
+// - Перевести до нижнього регістру настипні стрінгові значення
+// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+// let str1 = 'HELLO WORLD';
+// let str2 = 'LOREM IPSUM';
+// let str3 = 'JAVASCRIPT IS COOL';
+//
+// console.log(str1.toLowerCase());
+// console.log(str2.toLowerCase());
+// console.log(str3.toLowerCase());
+
+// - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+// let str = ' dirty string   ';
+// console.log(str.trim());
+
+// - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+//     let str = 'Ревуть воли як ясла повні';
+// let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
+//
+// let str = 'Ревуть воли як ясла повні';
+// let stringToArray = (str) => str.split(' ');
+// console.log(stringToArray(str));
+
+// - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
+// let arr = [10,8,-7,55,987,-1011,0,1050,0];
+//
+// console.log(arr.map(value => return value + ''));
+//console.log(arr.map(value => value.toString()));
+
+// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого,
+// або навпаки в залежності від значення аргументу direction.
+//     let nums = [11,21,3];
+// sortNums(nums,'ascending') // [3,11,21]
+// sortNums(nums,'descending') // [21,11,3]
+// let nums = [11,21,3];
+// let sortNums = (array, direction) =>{
+//     if(direction === 'ascending'){
+//         array.sort((a, b) => a - b)
+//     }
+//     else if (direction === 'descending'){
+//         array.sort((a, b) => b - a)
+//     }
+//     return array;
+// }
+// console.log(sortNums(nums, 'descending'));
+//console.log(sortNums(nums, 'ascending'));
+
+// - є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// // -- відсортувати його за спаданням за monthDuration
+// let sortDescenting = (array) => array.sort((a, b) => b.monthDuration - a.monthDuration);
+// console.log(sortDescenting(coursesAndDurationArray));
+//
+// // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+// let filterByLenth = (array) => array.filter(value => value.monthDuration > 5);
+// console.log(filterByLenth(coursesAndDurationArray));
+
+//
+// описати колоду карт
+// - знайти піковий туз
+// - всі шістки
+// - всі червоні карти
+// - всі буби
+// - всі трефи від 9 та більше
+//
+//
+// {
+//     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
+//         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
+//     color:'', // 'red','black'
+// }
+
+// let deckOfCards = [
+//     {
+//         cardSuit: null,
+//         value: 'Joker',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: null,
+//         value: 'Joker',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: '6',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: '7',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: '8',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: '9',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: '10',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: 'ace',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: 'jack',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: 'queen',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'spade',
+//         value: 'king',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: '6',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: '7',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: '8',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: '9',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: '10',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: 'ace',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: 'jack',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: 'queen',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'clubs',
+//         value: 'king',
+//         color: 'black'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: '6',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: '7',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: '8',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: '9',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: '10',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: 'ace',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: 'jack',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: 'queen',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'heart',
+//         value: 'king',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: '6',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: '7',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: '8',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: '9',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: '10',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: 'ace',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: 'jack',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: 'queen',
+//         color: 'red'
+//     },
+//     {
+//         cardSuit: 'diamond',
+//         value: 'king',
+//         color: 'red'
+//     }
+// ]
+
+// // - знайти піковий туз
+// console.log(deckOfCards.find(value => value.cardSuit === 'clubs' && value.value === 'ace'));
+// // - всі шістки
+// console.log(deckOfCards.filter(value => value.value === '6'));
+// // - всі червоні карти
+// console.log(deckOfCards.filter(value => value.color === 'red'));
+// // - всі буби
+// console.log(deckOfCards.filter(value => value.cardSuit === 'diamond'));
+// // - всі трефи від 9 та більше
+// console.log(deckOfCards.filter(value => value.cardSuit === 'spade' && value.value > '8' ||
+//     value.value === '10' && value.cardSuit ==='spade'));
+
